d4f7563831bcd5fa192403cc9cff059f5ddfae2f
Vitor-Calixto-NAVE <vitor.calixto@nave.org.br>
2026-03-20 11:04:23 -0300

    atividade 1 incompleta

--- ex-01-lacos-while.js
+++ ex-01-lacos-while.js
@@ -174,16 +174,26 @@ Crie um laço de repetição while que armazene em uma variável a soma dos núm
 de 1 a 32. Envie o valor resultante na variável resposta01.
 */
 // Escreva o código da solução abaixo:
-
-export const resposta01 = false
+let a = 0
+let c = 1
+while (soma1 <= 31){
+  a += c
+  c += 2
+  console.log (a)
+}
+export const resposta01 = a
 
 /* Questão 02
 Utilizando um laço de repetição while, calcule a soma de todos os números ímpares
 de 1 até 50 (incluindo 50). Armazene o resultado da soma na variável resposta02.
 */
 // Escreva o código da solução abaixo:
-
-export const resposta02 = false
+while (c <= 50){
+  a += c
+  c += 2
+  console.log(a)
+} 
+export const resposta02 = a
 
 /* Questão 03
 Crie um laço de repetição while que conte quantos números negativos existem no
@@ -192,6 +202,13 @@ array abaixo. Armazene a quantidade encontrada na variável resposta03.
 const numeros = [15, -3, 8, -12, 0, -7, 22, -1, 9, -5, 18, -2]
 */
 // Escreva o código da solução abaixo:
+const numeros = [15, -3, 8, -12, 0, -7, 22, -1, 9, -5, 18, -2]
+let indice = 0
+
+while (indice < numeros.length) {
+  console.log(numeros[indice])
+  indice = indice + 1
+}
 
 export const resposta03 = false
 
