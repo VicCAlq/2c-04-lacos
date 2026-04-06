26db042b96134d568a331f82c5f5512c8e2cf758
pedro-victor-nave <pedro.victor@nave.org.br>
2026-04-06 09:38:59 -0300

    Update ex-01-lacos-while.js

--- ex-01-lacos-while.js
+++ ex-01-lacos-while.js
@@ -250,10 +250,21 @@ na variável resposta05.
 */
 // Escreva o código da solução abaixo:
 let nomes1 = []
-let fim = 0
-while(fim <= 9){
-  nomes1 += nomes.pop()
-  fim++
+let order = 0;
+
+while(order <= nomes.length - 1){
+  order++;
+  let nome1 = nomes.pop()
+  let nome2 = nomes.pop()
+  let nome3 = nomes.pop()
+  let nome4 = nomes.pop()
+  let nome5 = nomes.pop()
+  let nome6 = nomes.pop()
+  let nome7 = nomes.pop()
+  let nome8 = nomes.pop()
+  let nome9 = nomes.pop()
+  let nome10 = nomes.pop()
+  nomes1.push(nome1,nome2 ,nome3,nome4,nome5,nome6,nome7,nome8,nome9,nome10);
 }
 
 export const resposta05 = nomes1
